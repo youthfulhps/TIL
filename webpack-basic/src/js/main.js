97c44221725aa4d4071b18528016038681effbbf
BH94 <ybh942002@gmail.com>
2021-04-28 23:14:18 +0900

    Create bundle.js including polyfill using webpack

--- webpack-basic/src/js/main.js
+++ webpack-basic/src/js/main.js
@@ -1,1 +1,17 @@
+// import '@babel/polyfill';  //for ES6
+
 console.log("getting-started-with-webpack!");
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("cool!");
+  }, 1000);
+});
+
+console.log(promise);
+
+const obj = Object.assign({}, { a: 1 }, { a: 2 });
+
+console.log(obj);
+
+console.log(Array.from([1, 2, 3], (v) => v + v));
