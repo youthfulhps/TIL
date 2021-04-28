07b2ad6c8a3e2db163f1844bf8718b5caf87e3b9
BH94 <ybh942002@gmail.com>
2021-04-28 22:50:59 +0900

    Create bundle.js using webpack

---
+++ webpack-basic/dist/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log("getting-started-with-webpack!");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
