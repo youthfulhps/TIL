1f9696ea2a5c8e5abf9780f2a57757035cfe6623
BH94 <ybh942002@gmail.com>
2021-05-02 15:00:56 +0900

    Implement input handler using recoil

--- react-recoil/src/App.tsx
+++ react-recoil/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useRecoilState } from "recoil";
+import { textState } from "./atoms/text";
 
 function App() {
+  const [text, setText] = useRecoilState(textState);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{text}</h1>
+      <input type="text" onChange={handleChangeInput} />
     </div>
   );
 }
